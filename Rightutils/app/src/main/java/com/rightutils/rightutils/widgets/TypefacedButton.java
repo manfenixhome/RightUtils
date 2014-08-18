c2fb10f7fbf30552326910b3c4c272600dc23144
Anton Maniskevich <fenixhomepro@gmail.com>
2014-08-18 11:21:26 +0300

    Added typeface widgets

---
+++ Rightutils/app/src/main/java/com/rightutils/rightutils/widgets/TypefacedButton.java
@@ -0,0 +1,43 @@
+package com.rightutils.rightutils.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.rightutils.rightutils.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Anton Maniskevich on 8/11/14.
+ */
+public class TypefacedButton extends Button {
+
+	private static Map<String, Typeface> fonts = new HashMap<String, Typeface>();
+
+	public TypefacedButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		if (isInEditMode()) {
+			return;
+		}
+
+		TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.TypefacedTextView);
+		String fontName = styledAttrs.getString(R.styleable.TypefacedTextView_typeface);
+		styledAttrs.recycle();
+
+		if (fontName != null) {
+			if (!fonts.containsKey(fontName)) {
+				Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+				setTypeface(typeface);
+				fonts.put(fontName, typeface);
+			} else {
+				setTypeface(fonts.get(fontName));
+			}
+		}
+	}
+}
---
+++ Rightutils/app/src/main/java/com/rightutils/rightutils/widgets/TypefacedEditText.java
@@ -0,0 +1,41 @@
+package com.rightutils.rightutils.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import com.rightutils.rightutils.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Anton Maniskevich on 8/11/14.
+ */
+public class TypefacedEditText extends EditText {
+	private static Map<String, Typeface> fonts = new HashMap<String, Typeface>();
+
+	public TypefacedEditText(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		if (isInEditMode()) {
+			return;
+		}
+
+		TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.TypefacedTextView);
+		String fontName = styledAttrs.getString(R.styleable.TypefacedTextView_typeface);
+		styledAttrs.recycle();
+
+		if (fontName != null) {
+			if (!fonts.containsKey(fontName)) {
+				Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+				setTypeface(typeface);
+				fonts.put(fontName, typeface);
+			} else {
+				setTypeface(fonts.get(fontName));
+			}
+		}
+	}
+}
